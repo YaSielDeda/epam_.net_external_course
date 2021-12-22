59387755844b64c52404c5d833803f1a76fa059b
YaHochuPitsi <bereg6484@gmail.com>
2021-12-22 14:12:44 +0400

    Task 1.2 full

---
+++ Task 1/Task_1.2/1.2.2. DOUBLER/1.2.2. DOUBLER/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _1._2._2._DOUBLER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input string: ");
+            string inputString = Console.ReadLine();
+
+            Console.Write("Input doubler string: ");
+            string doublerString = Console.ReadLine();
+
+            foreach (char item in doublerString.Distinct())
+            {
+                if (inputString.Contains(item))
+                    inputString = inputString.Replace(item.ToString(), item.ToString() + item.ToString());
+            }
+
+            Console.WriteLine($"Result: {inputString}");
+        }
+    }
+}
---
+++ Task 1/Task_1.2/1.2.3. LOWERCASE/Program.cs
@@ -0,0 +1,24 @@
+﻿using Dolgov_Task_1._2;
+using System;
+using System.Linq;
+
+namespace _1._2._3._LOWERCASE
+{
+    class Program
+    {
+        //additional task
+        static void Main(string[] args)
+        {
+            string fileName = "text.txt";
+            string text = FileReader.ReadFile(fileName);
+
+            char[] separators = { ',', ';', ':', ' ', '.' };
+
+            string[] spliitedText = text.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                ;
+
+            int numberOfLowerCaseWords = spliitedText.Where(x => x[0].ToString() == x[0].ToString().ToLower()).Count();
+            Console.WriteLine(numberOfLowerCaseWords);
+        }
+    }
+}
