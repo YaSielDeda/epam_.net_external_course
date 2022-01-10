919442bae5a32f45eb6f24e888af00b88aa491b8
YaHochuPitsi <bereg6484@gmail.com>
2022-01-10 22:50:28 +0400

    text input was changed to console input

---
+++ Task 1/Task 1.2/1.2.1. AVERAGES/1.2.1. AVERAGES/Program.cs
@@ -0,0 +1,20 @@
+﻿using Dolgov_Task_1._2;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _1._2._1._AVERAGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter sentence: ");
+            string[] arrFromText = Console.ReadLine().Split();
+
+            // Целочисленное среднее
+            int average = arrFromText.Sum(x => x.Length) / arrFromText.Length;
+            Console.WriteLine(average);
+        }
+    }
+}
