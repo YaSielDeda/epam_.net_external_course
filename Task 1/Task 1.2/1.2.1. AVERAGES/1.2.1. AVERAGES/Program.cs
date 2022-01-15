aff1c5f1a19ac3ae3e1175c94cd93a4cd7bba970
YaHochuPitsi <bereg6484@gmail.com>
2022-01-15 13:22:14 +0400

    Now punctuation symbols aren't considering at average value calculation

--- Task 1/Task 1.2/1.2.1. AVERAGES/1.2.1. AVERAGES/Program.cs
+++ Task 1/Task 1.2/1.2.1. AVERAGES/1.2.1. AVERAGES/Program.cs
@@ -9,8 +9,18 @@ namespace _1._2._1._AVERAGES
     {
         static void Main(string[] args)
         {
+            string[] separators = new[] { "!", "?", ".", "," };
+
             Console.Write("Enter sentence: ");
             string[] arrFromText = Console.ReadLine().Split();
+
+            for (int i = 0; i < arrFromText.Length; i++)
+            {
+                for (int j = 0; j < separators.Length; j++)
+                {
+                    arrFromText[i] = arrFromText[i].Replace(separators[j], "");
+                }
+            }
 
             // Целочисленное среднее
             int average = arrFromText.Sum(x => x.Length) / arrFromText.Length;
