8dad374c1210fc373999be87364860633d1f79d6
YaSielDeda <73429112+YaSielDeda@users.noreply.github.com>
2022-01-16 12:39:12 +0400

    text input was changed to console input (#2)
    * text input was changed to console input
    
    * Now punctuation symbols aren't considering at average value calculation
    
    * Now ";" at the right place
    
    * Method name has been fixed and now it's returning new instance of stringbuilder

---
+++ Task 1/Task 1.2/1.2.3. LOWERCASE/Program.cs
@@ -0,0 +1,23 @@
+﻿using Dolgov_Task_1._2;
+using System;
+using System.Linq;
+
+namespace _1._2._3._LOWERCASE
+{
+    class Program
+    {
+        //additional task
+        static void Main(string[] args)
+        {
+            Console.Write("Input text: ");
+            string text = Console.ReadLine();
+
+            char[] separators = { ',', ';', ':', ' ', '.' };
+
+            string[] spliitedText = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            int numberOfLowerCaseWords = spliitedText.Where(x => x[0].ToString() == x[0].ToString().ToLower()).Count();
+            Console.WriteLine(numberOfLowerCaseWords);
+        }
+    }
+}
