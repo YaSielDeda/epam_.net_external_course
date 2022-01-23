d49cf6ded5ecdf265ade27bfc03b8f5b6b28a3ba
YaHochuPitsi <bereg6484@gmail.com>
2022-01-23 17:18:35 +0400

    Task_3.1._Weakest_Text full

---
+++ Task 3/Task 3.1/3.1.1. WEAKEST LINK/Program.cs
@@ -0,0 +1,44 @@
+﻿using Logging;
+using Serilog;
+using System;
+
+namespace _3._1._1._WEAKEST_LINK
+{
+    class Program
+    {
+        static void Main()
+        {
+            Logger.InitializeConsoleLogger();
+
+            Log.Information("Input N: ");
+            int.TryParse(Console.ReadLine(), out int N);
+
+            Log.Information("Enter which person number will be removed at every lap: ");
+            int.TryParse(Console.ReadLine(), out int removableNumber);
+
+            try
+            {
+                if (N == 0 || removableNumber == 0)
+                    throw new ArgumentException("You have entered invalid data!");
+
+                if (removableNumber > N)
+                    throw new ArgumentException("Removable number can't be more than N!");
+
+                int count = 1;
+
+                while (N >= removableNumber - 1)
+                {
+                    Log.Information($"Round {count}. Man is removed, {N} remaining");
+                    N--;
+                    count++;
+                }
+
+                Log.Information("Game over. Impossible to remove more.");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(messageTemplate: ex.Message);
+            }
+        }
+    }
+}
---
+++ Task 3/Task 3.1/3.1.2. TEXT ANALYSIS/Program.cs
@@ -0,0 +1,57 @@
+﻿using Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._1._2._TEXT_ANALYSIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.InitializeConsoleLogger();
+
+            Log.Information("Input text and then press Enter:");
+
+            try
+            {
+                char[] separators = { ',', ';', ':', ' ', '.', '-', '!', '?', '(', ')', '\\', '/' };
+                List<string> arr = new(Console.ReadLine().ToLower().Trim().Split(separators, StringSplitOptions.RemoveEmptyEntries));
+
+                if (arr.Count == 0)
+                    throw new ArgumentNullException();
+
+                List<KeyValuePair<int, string>> keyWordValueTimes = new();
+
+                foreach (var word in arr.Distinct())
+                    keyWordValueTimes.Add(new KeyValuePair<int, string>(arr.Where(x => x == word).Count(), word));
+
+                var sortedDictionary = from entry in keyWordValueTimes 
+                                       orderby entry.Key descending select entry;
+
+                bool hasManyRecurringWords = false;
+
+                foreach (var pair in sortedDictionary)
+                {
+                    if (pair.Key >= 4)
+                    {
+                        Log.Warning($"{pair.Key} times - {pair.Value}");
+                        hasManyRecurringWords = true;
+                    }
+                    else
+                        Log.Information($"{pair.Key} times - {pair.Value}");
+                }
+
+                if(hasManyRecurringWords)
+                    Log.Information("Not bad, but better try to replace some of recurring words with synonims");
+                else
+                    Log.Information($"Nice, your text looks clean from recurring words!");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }   
+        }
+    }
+}
